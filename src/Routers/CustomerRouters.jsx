5c311a3f30c6bb9d1652434080863df51c557a3c
sheetal0797 <sheetal0797@gmail.com>
2024-04-03 23:12:02 +0530

    deploying code in vercel

--- src/Routers/CustomerRouters.jsx
+++ src/Routers/CustomerRouters.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import HomePage from "../customer/pages/HomePage/HomePage";
 import Cart from "../customer/components/Cart/Cart";
 import Navigation from "../customer/components/Navigation/Navigation";
 import Footer from "../customer/components/Footer/Footer";
@@ -10,6 +9,7 @@ import Checkout from "../customer/components/Checkout/Checkout";
 import Order from "../customer/components/Order/Order";
 import OrderDetails from "../customer/components/Order/OrderDetails";
 import PaymentSuccess from "../customer/components/Payment/PaymentSuccess";
+import HomePage from "../customer/pages/HomePage/HomePage";
 
 const CustomerRouters = () => {
   return (
